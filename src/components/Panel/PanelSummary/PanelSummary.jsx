04499c6b324d3d7b87c47681a38713496006e2b1
Richard Rodrigues <richardrodriguesdossantosu@gmail.com>
2026-05-03 12:33:26 -0300

    styles: Adição de variaveis no root, ajuste de responsividade e ajustes finos de layout

--- src/components/Panel/PanelSummary/PanelSummary.jsx
+++ src/components/Panel/PanelSummary/PanelSummary.jsx
@@ -13,28 +13,28 @@ export default function PanelSummary({pedidos, faturamento, pedidosAtivos}){
                         <small className='text-secondary fs-6'>Pedidos hoje</small>
                         <h2>{pedidos}</h2>
                     </div>
-                    <div className='icon-card'>
-                        <i class="fa-solid fa-bag-shopping"></i>
+                    <div className='icon-card icon-card-green'>
+                        <i className="fa-solid fa-bag-shopping"></i>
                     </div>
                 </div>
 
                 <div className="card-item d-flex justify-content-around align-items-center gap-4">
                     <div className="d-flex flex-column">
-                        <small className='text-secondary fs-6'>Pedidos hoje</small>
+                        <small className='text-secondary fs-6'>Faturamento</small>
                         <h2>{faturamento}</h2>
                     </div>
-                    <div className='icon-card'>
-                        <i class="fa-solid fa-bag-shopping"></i>
+                    <div className='icon-card icon-card-orange'>
+                        <i className="bx bx-dollar" />
                     </div>
                 </div>
 
                 <div className="card-item d-flex justify-content-around align-items-center gap-4">
                     <div className="d-flex flex-column">
-                        <small className='text-secondary fs-6'>Pedidos hoje</small>
+                        <small className='text-secondary fs-6'>Pedidos Ativos</small>
                         <h2>{pedidosAtivos}</h2>
                     </div>
-                    <div className='icon-card'>
-                        <i class="fa-solid fa-bag-shopping"></i>
+                    <div className='icon-card icon-card-blue'>
+                        <i className="fa-regular fa-clock"></i>
                     </div>
                 </div>
             </section>
